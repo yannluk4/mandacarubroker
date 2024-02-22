1d6b51665bc795f5ed59a0d97f4ba4f69bb28277
yannluk4 <yannluccayl@outlook.com>
2024-02-22 19:02:20 -0300

    Adicionando o java docs

--- src/main/java/com/mandacarubroker/MandacarubrokerApplication.java
+++ src/main/java/com/mandacarubroker/MandacarubrokerApplication.java
@@ -2,12 +2,21 @@ package com.mandacarubroker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * The main application class for Mandacarubroker.
+ * This class initializes and runs the Mandacarubroker application using Spring Boot.
+ */
 @SpringBootApplication
 public class MandacarubrokerApplication {
 
-	public static void main(String[] args) {
+	/**
+	 * The main method of the Mandacarubroker application.
+	 * This method initializes and starts the Spring Boot application.
+	 *
+	 * @param args Command line arguments passed to the application.
+	 */
+	public static void main(final String[] args) {
 		SpringApplication.run(MandacarubrokerApplication.class, args);
 	}
 
